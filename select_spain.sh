d5151f181a3511eabf82b1e06443b9d97b443c27
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-04-21 14:23:40 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-21042021
     Your branch is up to date with 'origin/taglio-21042021'.
    
     Changes to be committed:
    	new file:   results.txt
    	modified:   select_spain.sh
    	modified:   setup_geodatawww

--- select_spain.sh
+++ select_spain.sh
@@ -1,7 +1,7 @@
 #!/bin/ksh
 
 # Latitud 35, 43
-# Longitud 3, 9
+# Longitud 0, 3 E - 0 , 9 W
 
 if [ -e "results.txt" ]; then
 	rm -rf results.txt
@@ -12,13 +12,26 @@ fi
 filepath=
 file=
 nordwest=
+nordeast=
 for filepath in */*; do
 	file=$(basename "$filepath") 
 	nordwest=$(echo $file | grep -v "S" | grep -v "E")
 	if [[ ! -z $nordwest ]]; then
 		typeset -i lat=$(echo $nordwest | sed -e "s/.*N//g" -e "s/W.*//g")
 		typeset -i long=$(echo $nordwest | sed -e "s/.*W//g" -e "s/\..*//g")
-		if [[ $lat -ge 35 && $lat -le 43 && $long -ge 3 && $long -le 9 ]]; then
+		if [[ $lat -ge 35 && $lat -le 43 && $long -ge 0 && $long -le 9 ]]; then
+			if [[ "$filepath" == SRTM3* ]] then
+				mv $filepath "/home/riccardo/.wine/drive_c/Radio_Mobile/Geodata/srtm3/"
+			else
+				mv $filepath "/home/riccardo/.wine/drive_c/Radio_Mobile/Geodata/Landcover/"
+			fi
+		fi
+	fi
+	nordeast=$(echo $file | grep -v "S" | grep -v "W")
+	if [[ ! -z $nordeast ]]; then
+		typeset -i lat=$(echo $nordeast | sed -e "s/.*N//g" -e "s/E.*//g")
+		typeset -i long=$(echo $nordeast | sed -e "s/.*E//g" -e "s/\..*//g")
+		if [[ $lat -ge 35 && $lat -le 43 && $long -ge 0 && $long -le 3 ]]; then
 			if [[ "$filepath" == SRTM3* ]] then
 				mv $filepath "/home/riccardo/.wine/drive_c/Radio_Mobile/Geodata/srtm3/"
 			else
